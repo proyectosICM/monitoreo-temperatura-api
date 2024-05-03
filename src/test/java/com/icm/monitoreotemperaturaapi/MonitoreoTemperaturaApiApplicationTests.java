b6bc44770ead39d69e4e5dc88bd0ffbc8c1d22ce
Eduardo Aguilar <es123y123@gmail.com>
2024-05-03 17:34:25 -0500

    C

---
+++ src/test/java/com/icm/monitoreotemperaturaapi/MonitoreoTemperaturaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icm.monitoreotemperaturaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoTemperaturaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
